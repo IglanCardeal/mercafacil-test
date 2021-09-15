ff676c528410ea80bf9889f2a50c5ea3af58ff54
Iglan Cardeal <cmtcardeal@outlook.com>
2021-09-15 13:47:21 -0300

    test: ensure signup service returns an error if client type is not allowed

--- src/modules/client/signup/test/signup-service.test.ts
+++ src/modules/client/signup/test/signup-service.test.ts
@@ -1,5 +1,23 @@
+import { Result } from '@src/shared/result/result';
+import { ClientDTO } from '../client-dto';
+import { SignUpService } from '../signup-service';
+
+const sutFactory = () => {
+  return {
+    sut: new SignUpService(),
+  };
+};
+
 describe('Client SignUp Service', () => {
-  it.skip('Should return 400 if the type of client is not allowed', async () => {
-    expect(1).toBe(1);
+  it('Should return an error if the type of client is not allowed', async () => {
+    const { sut } = sutFactory();
+    const clientData: ClientDTO = {
+      name: 'any name',
+      email: 'any email',
+      password: 'any_pass',
+      type: 'incorrect type',
+    };
+    const response: Result<any> = await sut.execute(clientData);
+    expect(response.isFailure).toBe(true);
   });
 });
