cee353e33e17fe72d016d487ed295167dccaa228
IglanCardeal <cmtcardeal@outlook.com>
2021-09-15 11:15:53 -0300

    test: ensure signup controller returns 400 if no type is provided

--- src/modules/client/signup/test/signup-controller.test.ts
+++ src/modules/client/signup/test/signup-controller.test.ts
@@ -18,4 +18,18 @@ describe('Client SignUp Controller', () => {
     expect(response.statusCode).toBe(400);
     expect(response.body).toBe('Missing param: name');
   });
+
+  it('Should return 400 if no type is provided', async () => {
+    const { sut } = sutFactory();
+    const request = {
+      body: {
+        name: 'any name',
+        type: '',
+        password: 'any_pass'
+      }
+    };
+    const response = await sut.handle(request);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toBe('Missing param: type');
+  });
 });
