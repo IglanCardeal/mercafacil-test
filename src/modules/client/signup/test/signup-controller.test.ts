82cbc7dcb9bc50a5d00fffeac6d7f088ab7d68c7
IglanCardeal <cmtcardeal@outlook.com>
2021-09-15 11:10:06 -0300

    test: ensure signup controller returns 400 if no name is provided

---
+++ src/modules/client/signup/test/signup-controller.test.ts
@@ -0,0 +1,21 @@
+import { SignUpController } from "../signup-controller";
+
+const sutFactory = () => {
+  return { sut: new SignUpController() };
+};
+
+describe('Client SignUp Controller', () => {
+  it('Should return 400 if no name is provided', async () => {
+    const { sut } = sutFactory();
+    const request = {
+      body: {
+        name: '',
+        type: 'varejao',
+        password: 'any_pass'
+      }
+    };
+    const response = await sut.handle(request);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toBe('Missing param: name');
+  });
+});
