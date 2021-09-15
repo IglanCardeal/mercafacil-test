7d9fdb260f72247f41adfdad3ac703010c366a54
Iglan Cardeal <cmtcardeal@outlook.com>
2021-09-15 13:39:47 -0300

    feat: create and using client DTO

---
+++ src/modules/client/signup/client-dto.ts
@@ -0,0 +1,6 @@
+export interface ClientDTO {
+  name: string;
+  email: string;
+  password: string;
+  type: string;
+}
--- src/modules/client/signup/signup-service.ts
+++ src/modules/client/signup/signup-service.ts
@@ -1,7 +1,10 @@
 import { ClientSignUpService } from '@src/domain/services/client-signup-service';
+import { Result } from '@src/shared/result/result';
+import { ClientDTO } from './client-dto';
 
 export class SignUpService implements ClientSignUpService {
-  async execute(data: any): Promise<any> {
+  async execute(data: ClientDTO): Promise<Result<any>> {
     data
+    return Result.ok({});
   }
 }
