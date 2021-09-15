7d1398edf11e8c1f0a1f8074f705d39ca21ddd17
Iglan Cardeal <cmtcardeal@outlook.com>
2021-09-15 11:09:27 -0300

    feat: ensure signup controller returns 500 when signup service throws

--- src/modules/client/signup/signup-controller.ts
+++ src/modules/client/signup/signup-controller.ts
@@ -14,35 +14,39 @@ export class SignUpController implements Controller {
   constructor(private readonly signupService: SignUpService) {}
 
   async handle(request: Request): Promise<Response> {
-    const requiredFields = ['name', 'type', 'password', 'email'];
-    for (const field of requiredFields) {
-      if (!request.body[field]) {
-        return badRequest(new MissingParamError(field));
+    try {
+      const requiredFields = ['name', 'type', 'password', 'email'];
+      for (const field of requiredFields) {
+        if (!request.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
-    }
-    const { type, email, password, name } = request.body;
-    const isValidClientType = Boolean(
-      ClientType.macapa === type || ClientType.varejao === type
-    );
-    if (!isValidClientType) {
-      return badRequest(
-        new DomainError(
-          `Invalid client type: ${type}. Must be "varejao" or "macapa"`
-        )
+      const { type, email, password, name } = request.body;
+      const isValidClientType = Boolean(
+        ClientType.macapa === type || ClientType.varejao === type
       );
+      if (!isValidClientType) {
+        return badRequest(
+          new DomainError(
+            `Invalid client type: ${type}. Must be "varejao" or "macapa"`
+          )
+        );
+      }
+      if (!email.includes('@')) {
+        return badRequest(new InvalidParamError('email'));
+      }
+      const client: Result<Client> = await this.signupService.execute({
+        type,
+        email,
+        password,
+        name,
+      });
+      if (client.isFailure) {
+        return badRequest(new DomainError(client.error));
+      }
+      return created({});
+    } catch (error) {
+      return { statusCode: 500, body: { error: 'Internal server error' } };
     }
-    if (!email.includes('@')) {
-      return badRequest(new InvalidParamError('email'));
-    }
-    const client: Result<Client> = await this.signupService.execute({
-      type,
-      email,
-      password,
-      name,
-    });
-    if (client.isFailure) {
-      return badRequest(new DomainError(client.error));
-    }
-    return created({});
   }
 }
