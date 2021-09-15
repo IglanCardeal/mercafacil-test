13204d72ff457973ca76b7c631a70485dae88197
Iglan Cardeal <cmtcardeal@outlook.com>
2021-09-15 13:46:01 -0300

    feat: ensure signup service returns an error if client type is not allowed

--- src/modules/client/signup/signup-service.ts
+++ src/modules/client/signup/signup-service.ts
@@ -1,10 +1,19 @@
+import { ClientType } from '@src/domain/models/client';
 import { ClientSignUpService } from '@src/domain/services/client-signup-service';
 import { Result } from '@src/shared/result/result';
 import { ClientDTO } from './client-dto';
 
 export class SignUpService implements ClientSignUpService {
-  async execute(data: ClientDTO): Promise<Result<any>> {
-    data
+  async execute(clientData: ClientDTO): Promise<Result<any>> {
+    const { type } = clientData;
+    const isValidClientType = Boolean(
+      ClientType.macapa === type || ClientType.varejao === type
+    );
+    if (!isValidClientType) {
+      return Result.fail(
+        `Invalid client type: ${type}. Must be "varejao" or "macapa"`
+      );
+    }
     return Result.ok({});
   }
 }
