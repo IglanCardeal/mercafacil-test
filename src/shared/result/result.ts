21d36e6b5629a941621d7d742dbdb6bf9afb11ba
IglanCardeal <cmtcardeal@outlook.com>
2021-09-16 16:13:00 -0300

    feat: create a type field to return more semantic domain errors

--- src/shared/result/result.ts
+++ src/shared/result/result.ts
@@ -2,9 +2,10 @@ export class Result<T> {
   public isSuccess: boolean;
   public isFailure: boolean;
   public error: string;
+  public type: string;
   private _value: T;
 
-  constructor (isSuccess: boolean, error?: string, value?: T) {
+  constructor(isSuccess: boolean, error?: string, value?: T, type?: string) {
     if (isSuccess && error) {
       throw new Error(
         'InvalidOperation: A result cannot be successful and contain an error'
@@ -19,21 +20,22 @@ export class Result<T> {
     this.isFailure = !isSuccess;
     this.error = error as string;
     this._value = value as T;
+    this.type = type as string;
     Object.freeze(this);
   }
 
-  public getValue (): T {
+  public getValue(): T {
     if (this.isFailure) {
       throw new Error('Cant retrieve the value from a failed result.');
     }
     return this._value;
   }
 
-  public static ok<U> (value?: U): Result<U> {
+  public static ok<U>(value?: U): Result<U> {
     return new Result(true, '', value);
   }
 
-  public static fail<U> (error: string): Result<U> {
-    return new Result(false, error);
+  public static fail<U>(error: string, type?: string): Result<U> {
+    return new Result(false, error, undefined as any, type);
   }
 }
