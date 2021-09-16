5432026c3e394193e39be43096a3a88eb5a2a36b
IglanCardeal <cmtcardeal@outlook.com>
2021-09-16 17:41:00 -0300

    test: ensure create service throws when client type respotiory does not exist

--- src/modules/contact/create/test/create-service.test.ts
+++ src/modules/contact/create/test/create-service.test.ts
@@ -1,10 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { clientTypesArray } from '@src/domain/models/client';
+import { Contact } from '@src/domain/models/contact';
+import { Result } from '@src/shared/result/result';
 import { CreateContactDTO } from '../create-contact-dto';
 import { CreateService } from '../create-service';
+import { CreateContactRepository } from '../ports';
+
+class CreateContactRepositoryStub implements CreateContactRepository {
+  public varejao = {
+    async createContact(contacts: any): Promise<Contact[]> {
+      return [] as any;
+    },
+  };
+
+  public macapa = {
+    async createContact(contacts: any): Promise<Contact[]> {
+      return [] as any;
+    },
+  };
+}
 
 const sutFactory = () => {
+  const createContactRepositoryStub = new CreateContactRepositoryStub();
   return {
-    sut: new CreateService(clientTypesArray),
+    sut: new CreateService(clientTypesArray, createContactRepositoryStub),
+    createContactRepositoryStub,
   };
 };
 
@@ -16,7 +36,20 @@ describe('Create Contact Service', () => {
       key: 'unique_key',
       type: 'invalid' as any,
     };
-    const response = await sut.execute(data);
+    const response: Result<Contact[]> = await sut.execute(data);
     expect(response.isFailure).toBe(true);
+  });
+
+  it('Should return failure when client type repository does not exist', async () => {
+    const { sut, createContactRepositoryStub } = sutFactory();
+    createContactRepositoryStub.macapa = {} as any;
+    const data: CreateContactDTO = {
+      contacts: [{ name: 'Any Name', cellphone: '5541999999999' }],
+      key: 'unique_key',
+      type: 'macapa',
+    };
+    await expect(sut.execute(data)).rejects.toThrowError(
+      `Client repository not found for type: ${data.type}`
+    );
   });
 });
