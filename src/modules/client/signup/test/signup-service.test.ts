14fd6f165e23ecc9e1b2222f79a923c026caf892
IglanCardeal <cmtcardeal@outlook.com>
2021-09-16 08:15:13 -0300

    test: ensure signup service throws if the repository does not exist

--- src/modules/client/signup/test/signup-service.test.ts
+++ src/modules/client/signup/test/signup-service.test.ts
@@ -1,30 +1,52 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Client, ClientTypes } from '@src/domain/models/client';
+import {
+  Client,
+  ClientTypes,
+  clientTypesArray,
+} from '@src/domain/models/client';
 import { Result } from '@src/shared/result/result';
 import { ClientDTO } from '../client-dto';
 import { ClientSignUpRepository } from '../ports';
 import { SignUpService } from '../signup-service';
 
 class ClientSignUpRepositoryStub implements ClientSignUpRepository {
-  async findClientByEmailAndType(
-    email: string,
-    clientType: string
-  ): Promise<Client> {
-    return {
-      name: 'any name',
-      email: 'any@email.com',
-      password: 'any_pass',
-      type: 'macapa',
-      id: 'any_id',
-      key: 'any_key',
-    };
-  }
+  public macapa = {
+    async findClientByEmailAndType(
+      email: string,
+      clientType: string
+    ): Promise<Client> {
+      return {
+        name: 'any name',
+        email: 'any@email.com',
+        password: 'any_pass',
+        type: 'macapa',
+        id: 'any_id',
+        key: 'any_key',
+      };
+    },
+  };
+
+  public varejao = {
+    async findClientByEmailAndType(
+      email: string,
+      clientType: string
+    ): Promise<Client> {
+      return {
+        name: 'any name',
+        email: 'any@email.com',
+        password: 'any_pass',
+        type: 'macapa',
+        id: 'any_id',
+        key: 'any_key',
+      };
+    },
+  };
 }
 
 const sutFactory = () => {
   const clientSignUpRepositoryStub = new ClientSignUpRepositoryStub();
   return {
-    sut: new SignUpService(clientSignUpRepositoryStub),
+    sut: new SignUpService(clientTypesArray, clientSignUpRepositoryStub),
     clientSignUpRepositoryStub,
   };
 };
@@ -36,13 +58,27 @@ describe('Client SignUp Service', () => {
       name: 'any name',
       email: 'any@email.com',
       password: 'any_pass',
-      type: 'incorrect type',
+      type: 'incorrect type' as ClientTypes,
     };
     const response: Result<any> = await sut.execute(clientData);
     expect(response.isFailure).toBe(true);
     expect(response.error).toBe(
       `Invalid client type: ${clientData.type}. Must be "varejao" or "macapa"`
     );
+  });
+
+  it.skip('Should an throw if the repository does not exist for the type of client', async () => {
+    const clientSignUpRepositoryStub = new ClientSignUpRepositoryStub();
+    const clientTypes = ['verejao', 'macapa'];
+    clientSignUpRepositoryStub.macapa = {} as any;
+    const sut = new SignUpService(clientTypes, clientSignUpRepositoryStub);
+    const clientData: ClientDTO = {
+      name: 'any name',
+      email: 'any@email.com',
+      password: 'any_pass',
+      type: 'macapa',
+    };
+    await expect(sut.execute(clientData)).rejects.toThrow();
   });
 
   it.skip('Should return an error if the email already exist', async () => {
