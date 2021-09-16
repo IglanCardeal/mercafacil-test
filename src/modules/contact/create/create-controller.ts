cf79dd47c632916ec9e3d37fbdc992adf230a596
IglanCardeal <cmtcardeal@outlook.com>
2021-09-16 16:33:04 -0300

    feat: ensure create controller returns 500 when create service throws

--- src/modules/contact/create/create-controller.ts
+++ src/modules/contact/create/create-controller.ts
@@ -5,7 +5,12 @@ import {
   InvalidParamError,
   MissingParamError,
 } from '@src/shared/errors';
-import { badRequest, created, unauthorized } from '@src/shared/http';
+import {
+  badRequest,
+  created,
+  internalServerError,
+  unauthorized,
+} from '@src/shared/http';
 import { Controller } from '@src/shared/ports/controller-port';
 import { Request, Response } from '@src/shared/ports/http-port';
 import { Result } from '@src/shared/result/result';
@@ -14,37 +19,40 @@ export class CreateController implements Controller {
   constructor(private readonly createService: CreateContactService) {}
 
   async handle(request: Request): Promise<Response> {
-    const { contacts, type, key } = request.body;
-    if (!contacts) {
-      return badRequest(new MissingParamError('contacts'));
-    }
-    const isArrayOfContacts = Boolean(request.body.contacts instanceof Array);
-    if (!isArrayOfContacts) {
-      return badRequest(new InvalidParamError('contacts must be an array'));
-    }
-    for (const contact of contacts) {
-      if (!contact.name) {
-        return badRequest(new MissingParamError('contact name'));
+    try {
+      const { contacts, type, key } = request.body;
+      if (!contacts) {
+        return badRequest(new MissingParamError('contacts'));
       }
-      if (!contact.cellphone) {
-        return badRequest(new MissingParamError('contact cellphone'));
+      const isArrayOfContacts = Boolean(request.body.contacts instanceof Array);
+      if (!isArrayOfContacts) {
+        return badRequest(new InvalidParamError('contacts must be an array'));
       }
+      for (const contact of contacts) {
+        if (!contact.name) {
+          return badRequest(new MissingParamError('contact name'));
+        }
+        if (!contact.cellphone) {
+          return badRequest(new MissingParamError('contact cellphone'));
+        }
+      }
+      const contactsCreated: Result<Contact[]> =
+        await this.createService.execute({
+          contacts,
+          type,
+          key,
+        });
+      if (contactsCreated.isFailure) {
+        const domainErrorType = contactsCreated.type;
+        const domainError = contactsCreated.error;
+        if (domainErrorType === 'unauthorized') {
+          return unauthorized(new DomainError(domainError));
+        }
+        return badRequest(new DomainError(domainError));
+      }
+      return created(contactsCreated);
+    } catch (error) {
+      return internalServerError();
     }
-    const contactsCreated: Result<Contact[]> = await this.createService.execute(
-      {
-        contacts,
-        type,
-        key,
-      }
-    );
-    if (contactsCreated.isFailure) {
-      const domainErrorType = contactsCreated.type;
-      const domainError = contactsCreated.error;
-      if (domainErrorType === 'unauthorized') {
-        return unauthorized(new DomainError(domainError));
-      }
-      return badRequest(new DomainError(domainError));
-    }
-    return created(contactsCreated);
   }
 }
