9d6bf7c91040d42e1c77e7e602e9b4aae093e204
IglanCardeal <cmtcardeal@outlook.com>
2021-09-16 15:19:13 -0300

    test: ensure create controller returns 400 when contacts is not an array

--- src/modules/contact/create/test/create-controller.test.ts
+++ src/modules/contact/create/test/create-controller.test.ts
@@ -18,4 +18,18 @@ describe('Create Contact Controller', () => {
     expect(response.statusCode).toBe(400);
     expect(response.body.error).toBe('Missing param: contacts');
   });
+
+  it('Should return 400 when contacts is not an array', async () => {
+    const { sut } = sutFactory();
+    const request = {
+      body: {
+        contacts: {},
+      },
+    };
+    const response = await sut.handle(request);
+    expect(response.statusCode).toBe(400);
+    expect(response.body.error).toBe(
+      'Invalid param: contacts must be an array'
+    );
+  });
 });
