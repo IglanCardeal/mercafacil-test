50a4a0ae58642a42de7ea498b562bc820879342f
IglanCardeal <cmtcardeal@outlook.com>
2021-09-16 09:41:08 -0300

    feat: ensure signin controller returns 400 if no email was found

---
+++ src/domain/services/client-signin-service.ts
@@ -0,0 +1,3 @@
+export interface ClientSignInService {
+  execute: (data: any) => Promise<any>;
+}
--- src/modules/client/signin/signin-controller.ts
+++ src/modules/client/signin/signin-controller.ts
@@ -1,10 +1,17 @@
-import { InvalidParamError, MissingParamError } from '@src/shared/errors';
+import { Client } from '@src/domain/models/client';
+import { ClientSignInService } from '@src/domain/services/client-signin-service';
+import {
+  DomainError,
+  InvalidParamError,
+  MissingParamError,
+} from '@src/shared/errors';
 import { badRequest, created, internalServerError } from '@src/shared/http';
 import { Controller } from '@src/shared/ports/controller-port';
 import { Request, Response } from '@src/shared/ports/http-port';
+import { Result } from '@src/shared/result/result';
 
 export class SignInController implements Controller {
-  // constructor() {} // private readonly signinService: ClientSignInService
+  constructor(private readonly signinService: ClientSignInService) {}
 
   async handle(request: Request): Promise<Response> {
     try {
@@ -14,10 +21,18 @@ export class SignInController implements Controller {
           return badRequest(new MissingParamError(field));
         }
       }
-      const { email } = request.body;
+      const { email, name, type } = request.body;
       // simples validação de email
       if (!email.includes('@')) {
         return badRequest(new InvalidParamError('email'));
+      }
+      const client: Result<Client> = await this.signinService.execute({
+        email,
+        name,
+        type,
+      });
+      if (client.isFailure) {
+        return badRequest(new DomainError(client.error));
       }
       return created({});
     } catch (error) {
