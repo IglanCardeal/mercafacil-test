48cfaf5773b050a0580b356a2264aa49e3ecee95
Iglan Cardeal <cmtcardeal@outlook.com>
2021-09-17 13:10:18 -0300

    refactor: remove conditional

--- src/infra/config/env.ts
+++ src/infra/config/env.ts
@@ -3,7 +3,7 @@ import { config } from 'dotenv';
 config();
 
 const ENV = {
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT,
   PRIVATE_KEY: process.env.PRIVATE_KEY,
   PUBLIC_KEY: process.env.PUBLIC_KEY,
