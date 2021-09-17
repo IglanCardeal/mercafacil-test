e391241364ff69b59e515b9de3114ca61ceb8a8a
IglanCardeal <cmtcardeal@outlook.com>
2021-09-16 23:06:36 -0300

    test: ensure uuid adapter calls uuidv4

---
+++ src/infra/adapters/uuidv4/uuid-adapter.test.ts
@@ -0,0 +1,15 @@
+import uuid from 'uuid';
+import { UuidAdapter } from './uuid-adapter';
+
+jest.mock('uuid', () => {
+  return {
+    v4: () => 'generated_id',
+  };
+});
+describe('UuidAdapter Adapter', () => {
+  it('should call uuidv4', () => {
+    const uuidv4Spy = jest.spyOn(uuid, 'v4');
+    new UuidAdapter().generate();
+    expect(uuidv4Spy).toHaveBeenCalled();
+  });
+});
