dc8d7d436252848fc35f5ce5379b135435422766
IglanCardeal <cmtcardeal@outlook.com>
2021-09-16 23:28:49 -0300

    test: ensure jwt adapter call jwt sign

---
+++ src/infra/adapters/jwt/jwt-adapter.test.ts
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import jsonwebtoken from 'jsonwebtoken';
+
+import * as JWTAdapter from './jwt-adapter';
+
+const { JsonWebTokenAdapter } = JWTAdapter;
+
+const signSpy = jest
+  .spyOn(jsonwebtoken, 'sign')
+  .mockImplementation((payload: any): string => {
+    return 'generate-token';
+  });
+
+const makeSut = () => {
+  return new JsonWebTokenAdapter();
+};
+
+describe('JWT Adapter', () => {
+  it('Should call jwt sign', () => {
+    const sut = makeSut();
+    sut.generate('any data');
+    expect(signSpy).toHaveBeenCalled();
+  });
+
+  // it('Should return a token', () => {
+  //   const sut = makeSut();
+  //   const token = sut.generate('any data');
+  //   expect(token).toBe('generate-token');
+  // });
+
+  // it('Should throw if jwt throws', () => {
+  //   const sut = makeSut();
+  //   jest.spyOn(JWTAdapter, 'generate').mockRestore();
+  //   expect(sut.generate(null)).rejects.toThrow();
+  // });
+
+  // it('Should return a production token', () => {
+  //   const sut = makeSut();
+  //   const token = sut.generate('any data');
+  //   expect(token).toBeTruthy();
+  // });
+
+  // it('Should validate the production token and return correct data', () => {
+  //   const sut = makeSut();
+  //   const token = sut.generate('any data');
+  //   const data = jsonwebtoken.verify(token, 'jest_any_secret');
+  //   expect(data).toBe('any data');
+  // });
+});
