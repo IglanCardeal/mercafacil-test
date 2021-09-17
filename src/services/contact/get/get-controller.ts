d964b82d0d32710f04121bb09473260ceab8b3ce
IglanCardeal <cmtcardeal@outlook.com>
2021-09-17 09:48:46 -0300

    feat: ensure get controller returns 500 when get service throws

--- src/services/contact/get/get-controller.ts
+++ src/services/contact/get/get-controller.ts
@@ -1,7 +1,12 @@
 import { Contact } from '@src/domain/models/contact';
 import { GetContactsService } from '@src/domain/services/contact';
 import { DomainError, MissingParamError } from '@src/shared/errors';
-import { badRequest, ok, unauthorized } from '@src/shared/http';
+import {
+  badRequest,
+  internalServerError,
+  ok,
+  unauthorized,
+} from '@src/shared/http';
 import { Controller } from '@src/shared/ports/controller-port';
 import { Request, Response } from '@src/shared/ports/http-port';
 import { Result } from '@src/shared/result/result';
@@ -10,25 +15,29 @@ export class GetController implements Controller {
   constructor(private readonly getService: GetContactsService) {}
 
   async handle(request: Request): Promise<Response> {
-    const requiredParams = ['key', 'type'];
-    for (const param of requiredParams) {
-      if (!request.body[param]) {
-        return badRequest(new MissingParamError(param));
+    try {
+      const requiredParams = ['key', 'type'];
+      for (const param of requiredParams) {
+        if (!request.body[param]) {
+          return badRequest(new MissingParamError(param));
+        }
       }
-    }
-    const { type, key } = request.body;
-    const contacts: Result<Contact[]> = await this.getService.execute({
-      type,
-      key,
-    });
-    if (contacts.isFailure) {
-      const domainErrorType = contacts.type;
-      const domainError = contacts.error;
-      if (domainErrorType === 'unauthorized') {
-        return unauthorized(new DomainError(domainError));
+      const { type, key } = request.body;
+      const contacts: Result<Contact[]> = await this.getService.execute({
+        type,
+        key,
+      });
+      if (contacts.isFailure) {
+        const domainErrorType = contacts.type;
+        const domainError = contacts.error;
+        if (domainErrorType === 'unauthorized') {
+          return unauthorized(new DomainError(domainError));
+        }
+        return badRequest(new DomainError(domainError));
       }
-      return badRequest(new DomainError(domainError));
+      return ok({ contacts: contacts.getValue() });
+    } catch (error) {
+      return internalServerError();
     }
-    return ok({ contacts: contacts.getValue() });
   }
 }
