97163d7abc4df8b309a76dc9909d6e8a6e4aedf5
IglanCardeal <cmtcardeal@outlook.com>
2021-09-17 12:15:55 -0300

    chore: add mysql env vars and drivers

--- src/infra/config/env.ts
+++ src/infra/config/env.ts
@@ -7,6 +7,12 @@ const ENV = {
   PORT: process.env.PORT,
   PRIVATE_KEY: process.env.PRIVATE_KEY,
   PUBLIC_KEY: process.env.PUBLIC_KEY,
+  // MySQL
+  MYSQL_ROOT_PASSWORD: process.env.MYSQL_ROOT_PASSWORD,
+  MYSQL_DATABASE: process.env.MYSQL_DATABASE,
+  MYSQL_USER: process.env.MYSQL_USER,
+  MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+  MYSQL_PORT: process.env.MYSQL_PORT,
 };
 
 Object.freeze(ENV);
