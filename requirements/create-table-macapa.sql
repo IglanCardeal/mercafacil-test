c4ac536709e7efff8c3127910619220ebda9de37
IglanCardeal <cmtcardeal@outlook.com>
2021-09-17 02:16:31 -0300

    chore: add requirements to gitignore and create docker compose

--- requirements/create-table-macapa.sql
+++
@@ -1,5 +0,0 @@
-CREATE table contacts (
-	id serial PRIMARY KEY,
-	nome VARCHAR ( 200 ) NOT NULL,
-	celular VARCHAR ( 20 ) NOT NULL
-);  
--- requirements/create-table-varejao.sql
+++
@@ -1,5 +0,0 @@
-CREATE table contacts (
-	id serial PRIMARY KEY,
-	nome VARCHAR ( 100 ) NOT NULL,
-	celular VARCHAR ( 13 ) NOT NULL
-);
