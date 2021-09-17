aeadda4fcefe9dd4e846a8e2f7fbda1e96e7912a
IglanCardeal <cmtcardeal@outlook.com>
2021-09-17 10:26:11 -0300

    feat: ensure get services returns failure when client key was not found

--- src/services/contact/get/get-service.ts
+++ src/services/contact/get/get-service.ts
@@ -1,18 +1,28 @@
 import { clientTypesArray } from '@src/domain/models/client';
 import { Contact } from '@src/domain/models/contact';
 import { GetContactsService } from '@src/domain/services/contact';
+
 import { Result } from '@src/shared/result/result';
-import { GetContactsDTO } from './ports';
+import { ClientRepository, GetContactsDTO } from './ports';
 
 export class GetContacts implements GetContactsService {
-  constructor(private readonly clientTypes = clientTypesArray) {}
+  private readonly clientTypes = clientTypesArray;
+
+  constructor(private readonly clientRepository: ClientRepository) {}
 
   async execute(data: GetContactsDTO): Promise<Result<Contact[]>> {
-    const { type } = data;
+    const { type, key } = data;
     const isClientTypeAllowed = this.clientTypes.includes(type);
     if (!isClientTypeAllowed) {
       return Result.fail(
         `Invalid client type: ${type}. Must be "varejao" or "macapa"`
+      );
+    }
+    const clientFound = await this.clientRepository[type].findClientByKey(key);
+    if (!clientFound) {
+      return Result.fail(
+        'Client not found. Action not authorized',
+        'unauthorized'
       );
     }
     return Result.ok([]);
--- src/services/contact/get/ports.ts
+++ src/services/contact/get/ports.ts
@@ -1,6 +1,16 @@
-import { ClientTypes } from '@src/domain/models/client';
+import { Client, ClientTypes } from '@src/domain/models/client';
 
 export interface GetContactsDTO {
   type: ClientTypes;
   key: string;
+}
+
+export interface ClientRepository {
+  varejao: {
+    findClientByKey: (key: string) => Promise<Client | null>;
+  };
+
+  macapa: {
+    findClientByKey: (key: string) => Promise<Client | null>;
+  };
 }
