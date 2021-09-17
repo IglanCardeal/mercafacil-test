885a134a1e8d4356d12503ee40eb5739af533951
IglanCardeal <cmtcardeal@outlook.com>
2021-09-17 01:37:31 -0300

    test: ensure get controller returns 401 when client key was not found

--- src/services/contact/get/test/get-controller.test.ts
+++ src/services/contact/get/test/get-controller.test.ts
@@ -1,8 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { GetContactService } from '@src/domain/services/contact';
+import { Result } from '@src/shared/result/result';
 import { GetController } from '../get-controller';
 
+class GetContactServiceStub implements GetContactService {
+  async execute(data: any): Promise<any> {
+    return Result.ok();
+  }
+}
+
 const sutFactory = () => {
+  const getContactServiceStub = new GetContactServiceStub();
   return {
-    sut: new GetController(),
+    sut: new GetController(getContactServiceStub),
+    getContactServiceStub,
   };
 };
 
@@ -31,5 +42,26 @@ describe('Get Contact Controller', () => {
     const response = await sut.handle(request);
     expect(response.statusCode).toBe(400);
     expect(response.body.error).toBe(`Missing param: key`);
+  });
+
+  it('Should return 401 when client key was not found', async () => {
+    const { sut, getContactServiceStub } = sutFactory();
+    const request = {
+      body: {
+        type: 'varejao',
+        key: 'key_not_exist',
+      },
+    };
+    jest
+      .spyOn(getContactServiceStub, 'execute')
+      .mockImplementationOnce(async () => {
+        return Result.fail(
+          `Client not found. Action not authorized`,
+          'unauthorized'
+        );
+      });
+    const response = await sut.handle(request);
+    expect(response.statusCode).toBe(401);
+    expect(response.body.error).toBe(`Client not found. Action not authorized`);
   });
 });
