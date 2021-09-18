4e0c0fe57999d13ed551dc905a0721dc221431b4
IglanCardeal <cmtcardeal@outlook.com>
2021-09-18 09:39:17 -0300

    feat: create contact repository

--- src/infra/repositories/contact-repository.ts
+++ src/infra/repositories/contact-repository.ts
@@ -1,17 +1,31 @@
 import { Contact } from '@src/domain/models/contact';
 import { CreateContactRepository } from '@src/repositories/contact-repository';
+import { MacapaContact, VarejaoContact } from '../models/contact-model';
 
 export class SequelizeContactRepository implements CreateContactRepository {
   public varejao = {
     createContact: async (
       contacts: Omit<Contact, 'id'>[]
     ): Promise<Contact[]> => {
-      contacts;
-      return [] as any;
+      const contactsCreated = await VarejaoContact.bulkCreate(contacts);
+      return contactsCreated.map((contact) => {
+        return {
+          id: contact.id,
+          name: contact.name,
+          cellphone: contact.cellphone,
+        };
+      });
     },
 
     getContacts: async (): Promise<Contact[]> => {
-      return [] as any;
+      const contacts = await VarejaoContact.findAll();
+      return contacts.map((contact) => {
+        return {
+          id: contact.id,
+          name: contact.name,
+          cellphone: contact.cellphone,
+        };
+      });
     },
   };
 
@@ -19,12 +33,25 @@ export class SequelizeContactRepository implements CreateContactRepository {
     createContact: async (
       contacts: Omit<Contact, 'id'>[]
     ): Promise<Contact[]> => {
-      contacts;
-      return [] as any;
+      const contactsCreated = await MacapaContact.bulkCreate(contacts);
+      return contactsCreated.map((contact) => {
+        return {
+          id: contact.id,
+          name: contact.name,
+          cellphone: contact.cellphone,
+        };
+      });
     },
 
     getContacts: async (): Promise<Contact[]> => {
-      return [] as any;
+      const contacts = await MacapaContact.findAll();
+      return contacts.map((contact) => {
+        return {
+          id: contact.id,
+          name: contact.name,
+          cellphone: contact.cellphone,
+        };
+      });
     },
   };
 }
