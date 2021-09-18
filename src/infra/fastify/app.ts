d1df7af60a5aad6c43cfafaaa4f7e77f2d164467
IglanCardeal <cmtcardeal@outlook.com>
2021-09-18 00:08:23 -0300

    feat: add module alias

--- src/infra/fastify/app.ts
+++ src/infra/fastify/app.ts
@@ -1,4 +1,8 @@
+import '../utils/module-alias';
+
+import { Response } from '@src/shared/ports/http-port';
 import Fastify, { FastifyInstance, FastifyReply } from 'fastify';
+import { signupFactory } from './signup-factory';
 
 const app: FastifyInstance = Fastify({
   logger: {
@@ -7,8 +11,17 @@ const app: FastifyInstance = Fastify({
   },
 });
 
-app.get('/ping', async (request, reply: FastifyReply) => {
-  return reply.send({ pong: 'it worked!' });
+app.post('/api/client/signup', async (request, reply: FastifyReply) => {
+  const clientData = request.body;
+  const signUpController = signupFactory();
+  const response: Response = await signUpController.handle({
+    body: clientData,
+  });
+  return reply.send(response);
 });
+
+// app.get('/ping', async (request, reply: FastifyReply) => {
+//   return reply.send({ pong: 'it worked!' });
+// });
 
 export { app };
---
+++ src/infra/utils/module-alias.ts
@@ -0,0 +1,8 @@
+import path from 'path';
+import moduleAlias from 'module-alias';
+
+const files = path.resolve(__dirname, '../../../');
+
+moduleAlias.addAliases({
+  '@src': path.join(files, 'src'),
+});
