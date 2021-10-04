e3f68323b3e5076ff7f2306fe2024ef875f3b129
IglanCardeal <cmtcardeal@outlook.com>
2021-10-03 23:33:54 -0300

    refactor: remove duplicated SignUpController tests code

--- src/services/client/signup/test/signup-controller.test.ts
+++ src/services/client/signup/test/signup-controller.test.ts
@@ -1,12 +1,27 @@
 import { ClientSignUpService } from '@src/domain/services/client';
+import {
+  DomainError,
+  InvalidParamError,
+  MissingParamError,
+} from '@src/shared/errors';
+import { badRequest, created, internalServerError } from '@src/shared/http';
 import { Result } from '@src/shared/result/result';
 import { SignUpController } from '../signup-controller';
 
 class SignUpServiceStub implements ClientSignUpService {
-  async execute(data: any): Promise<any> {
+  async execute (data: any): Promise<any> {
     return Result.ok<any>({ uuid: 'unique_key', id: 'unique_id', ...data });
   }
 }
+
+const makeRequest = () => ({
+  body: {
+    name: 'any name',
+    type: 'varejao',
+    password: 'any_pass',
+    email: 'any@email.com',
+  },
+});
 
 const sutFactory = () => {
   const signupService = new SignUpServiceStub();
@@ -16,92 +31,64 @@ const sutFactory = () => {
 describe('Client SignUp Controller', () => {
   it('Should return 400 if no name is provided', async () => {
     const { sut } = sutFactory();
-    const request = {
+    const response = await sut.handle({
       body: {
+        ...makeRequest().body,
         name: '',
-        type: 'varejao',
-        password: 'any_pass',
-        email: 'any@email.com',
       },
-    };
-    const response = await sut.handle(request);
-    expect(response.statusCode).toBe(400);
-    expect(response.body.error).toBe('Missing param: name');
+    });
+    expect(response).toEqual(badRequest(new MissingParamError('name')));
   });
 
   it('Should return 400 if no type is provided', async () => {
     const { sut } = sutFactory();
-    const request = {
+    const response = await sut.handle({
       body: {
-        name: 'any name',
+        ...makeRequest().body,
         type: '',
-        password: 'any_pass',
-        email: 'any@email.com',
       },
-    };
-    const response = await sut.handle(request);
-    expect(response.statusCode).toBe(400);
-    expect(response.body.error).toBe('Missing param: type');
+    });
+    expect(response).toEqual(badRequest(new MissingParamError('type')));
   });
 
   it('Should return 400 if no password is provided', async () => {
     const { sut } = sutFactory();
-    const request = {
+    const response = await sut.handle({
       body: {
-        name: 'any name',
-        type: 'varejao',
+        ...makeRequest().body,
         password: '',
-        email: 'any@email.com',
       },
-    };
-    const response = await sut.handle(request);
-    expect(response.statusCode).toBe(400);
-    expect(response.body.error).toBe('Missing param: password');
+    });
+    expect(response).toEqual(badRequest(new MissingParamError('password')));
   });
 
   it('Should return 400 if no email is provided', async () => {
     const { sut } = sutFactory();
-    const request = {
+    const response = await sut.handle({
       body: {
-        name: 'any name',
-        type: 'varejao',
-        password: 'any_pass',
+        ...makeRequest().body,
         email: '',
       },
-    };
-    const response = await sut.handle(request);
-    expect(response.statusCode).toBe(400);
-    expect(response.body.error).toBe('Missing param: email');
+    });
+    expect(response).toEqual(badRequest(new MissingParamError('email')));
   });
 
   it('Should return 400 if no email is invalid', async () => {
     const { sut } = sutFactory();
-    const request = {
+    const response = await sut.handle({
       body: {
-        name: 'any name',
-        type: 'varejao',
-        password: 'any_pass',
-        email: 'anyemail.com',
+        ...makeRequest().body,
+        email: 'invalidemail.com',
       },
-    };
-    const response = await sut.handle(request);
-    expect(response.statusCode).toBe(400);
-    expect(response.body.error).toBe('Invalid param: email');
+    });
+    expect(response).toEqual(badRequest(new InvalidParamError('email')));
   });
 
   it('Should call signup service with correct values', async () => {
     const { sut, signupService } = sutFactory();
-    const request = {
-      body: {
-        name: 'any name',
-        type: 'varejao',
-        password: 'any_pass',
-        email: 'any@email.com',
-      },
-    };
     const signupServiceSpy = jest.spyOn(signupService, 'execute');
-    await sut.handle(request);
-    expect(signupServiceSpy).toHaveBeenCalledWith(request.body);
+    await sut.handle(makeRequest());
+    expect(signupServiceSpy).toHaveBeenCalledWith(makeRequest().body);
   });
 
   it('Should return 400 when signup service returns a failure', async () => {
@@ -109,17 +96,10 @@ describe('Client SignUp Controller', () => {
     jest
       .spyOn(signupService, 'execute')
       .mockResolvedValueOnce(Result.fail('Random domain error'));
-    const request = {
-      body: {
-        name: 'any name',
-        type: 'varejao',
-        password: 'any_pass',
-        email: 'any@email.com',
-      },
-    };
-    const response = await sut.handle(request);
-    expect(response.statusCode).toBe(400);
-    expect(response.body.error).toBe('Random domain error');
+    const response = await sut.handle(makeRequest());
+    expect(response).toEqual(
+      badRequest(new DomainError('Random domain error'))
+    );
   });
 
   it('Should return 500 if signup service throws exception', async () => {
@@ -127,35 +107,19 @@ describe('Client SignUp Controller', () => {
     jest.spyOn(signupService, 'execute').mockImplementationOnce(async () => {
       throw new Error();
     });
-    const request = {
-      body: {
-        name: 'any name',
-        type: 'varejao',
-        password: 'any_pass',
-        email: 'any@email.com',
-      },
-    };
-    const response = await sut.handle(request);
-    expect(response.statusCode).toBe(500);
-    expect(response.body.error).toBe('Internal server error');
+    const response = await sut.handle(makeRequest());
+    expect(response).toEqual(internalServerError());
   });
 
   it('Should return 201 with correct client data when signup success', async () => {
     const { sut } = sutFactory();
-    const request = {
-      body: {
-        name: 'any name',
-        type: 'varejao',
-        password: 'any_pass',
-        email: 'any@email.com',
-      },
-    };
-    const response = await sut.handle(request);
-    expect(response.statusCode).toBe(201);
-    expect(response.body).toEqual({
-      ...request.body,
-      uuid: 'unique_key',
-      id: 'unique_id',
-    });
+    const response = await sut.handle(makeRequest());
+    expect(response).toEqual(
+      created({
+        ...makeRequest().body,
+        uuid: 'unique_key',
+        id: 'unique_id',
+      })
+    );
   });
 });
